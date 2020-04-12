454c7b4a99c95db5b069c1a8795839b0a7b9d820
Dillan J Gump <DJG@Josephs-MacBook-Air.local>
2020-04-12 14:41:34 -0400

    Adds a loop prompting multiple user inputs
    git status

--- textpro.py
+++ textpro.py
@@ -6,4 +6,14 @@ def sentence_maker(phrase):
     else:
         return "{}.".format(capitalized)
 
-print(sentence_maker("how are you"))
+
+results = []
+while True:
+    user_input = input("Say something: ")
+    if user_input == "\end":
+        break
+    else:
+        results.append(sentence_maker(user_input))
+
+print(results)
+
